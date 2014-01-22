3060fdefb860db51dcd2e5bbdd96f7b912586f88
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-01-22 20:40:43 +0100

    Branched from $/Docxes/Main

---
+++ Docxes/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Docxes")]
+[assembly: AssemblyProduct("Docxes")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyDescription("Management system for school data")]
+[assembly: AssemblyCompany("Dimitri Vranken, Nicola Bischof")]
+[assembly: AssemblyCopyright("Copyright © 2014, Dimitri Vranken & Nicola Bischof")]
