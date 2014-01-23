a59ea4f0852a36d939368452aab13d0364af376e
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-01-23 23:12:49 +0100

    Added the calendar project MonthViewCalendar (prototype)

---
+++ MonthViewCalendar/src/CalendarEntriesConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Collections.ObjectModel;
+
+namespace DimitriVranken.MonthViewCalendar {
+
+    /// <summary>
+    /// Gets the all of the <see cref="CalendarEntry" /> for the specified date.
+    /// </summary>
+    [ValueConversion(typeof(ObservableCollection<CalendarEvent>), typeof(ObservableCollection<CalendarEvent>))]
+    public class CalendarEntriesConverter : IMultiValueConverter {
+
+        #region Pubic Methods
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            DateTime date = (DateTime)values[1];
+
+            ObservableCollection<CalendarEvent> calendarEvents = new ObservableCollection<CalendarEvent>();
+            foreach (CalendarEvent calendarEvent in (ObservableCollection<CalendarEvent>)values[0]) {
+                if (calendarEvent.Date.Date == date) {
+                    calendarEvents.Add(calendarEvent);
+                }
+            }
+
+            return calendarEvents;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ MonthViewCalendar/src/CalendarEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DimitriVranken.MonthViewCalendar {
+
+    /// <summary>
+    /// An entry in a calandar.
+    /// </summary>
+    public class CalendarEvent {
+
+        #region Public Properties
+
+        public string Name { get; private set; }
+        public DateTime Date { get; private set; }
+
+        #endregion
+
+        #region Public Constructors
+
+        public CalendarEvent(string name, DateTime date) {
+            Name = name;
+            Date = date;
+        }
+
+        #endregion
+
+    }
+
+}
