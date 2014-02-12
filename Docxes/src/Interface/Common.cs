95968ab4c0eec428c36befbddf49fca0ca62de2f
Nicola Bischof <vizzart@hotmail.de>
2014-02-12 09:01:54 +0100

    Add AddToolTipp Method

--- Docxes/src/Interface/Common.cs
+++ Docxes/src/Interface/Common.cs
@@ -6,36 +6,52 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
-namespace VrankenBischof.Docxes.Interface {
+namespace VrankenBischof.Docxes.Interface
+{
 
-    /// <summary>
-    /// This class contains common functionality, which can be used in the context of a user interface.
-    /// </summary>
-    public static class Common {
+	/// <summary>
+	/// This class contains common functionality, which can be used in the context of a user interface.
+	/// </summary>
+	public static class Common
+	{
 
-        public static void ExtendWindowName(Window window) {
-            if (window == null) {
-                throw new ArgumentNullException("window");
-            }
+		public static void ExtendWindowName(Window window)
+		{
+			if (window == null)
+			{
+				throw new ArgumentNullException("window");
+			}
 
-            window.Title = Application.Current.MainWindow.GetType().Assembly.GetName().Name + " - " + window.Title;
-        }
+			window.Title = Application.Current.MainWindow.GetType().Assembly.GetName().Name + " - " + window.Title;
+		}
 
+		public static void AddToolTipToControl(Control control, String value)
+		{
+			if (control == null)
+			{
+				throw new ArgumentNullException("window");
+			}
 
-        public static bool AskForElementDeletion(string question, string objectType) {
-            if (String.IsNullOrEmpty(question)) {
-                throw new ArgumentNullException("question");
-            }
-            if (String.IsNullOrEmpty(objectType)) {
-                throw new ArgumentNullException("objectType");
-            }
+			control.ToolTip = value;
 
-            MessageBoxResult result = MessageBox.Show(question + Environment.NewLine +
-                                                      "Gelöschte Daten können nicht wiederhergestellt werden.",
-                                                      objectType + " wirklich löschen?", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No);
-            return result == MessageBoxResult.Yes;
-        }
+		}
+		public static bool AskForElementDeletion(string question, string objectType)
+		{
+			if (String.IsNullOrEmpty(question))
+			{
+				throw new ArgumentNullException("question");
+			}
+			if (String.IsNullOrEmpty(objectType))
+			{
+				throw new ArgumentNullException("objectType");
+			}
 
-    }
+			MessageBoxResult result = MessageBox.Show(question + Environment.NewLine +
+																								"Gelöschte Daten können nicht wiederhergestellt werden.",
+																								objectType + " wirklich löschen?", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.No);
+			return result == MessageBoxResult.Yes;
+		}
+
+	}
 
 }
