8f2b5ac6eee4b8b2b457c7caf9d1f2e264ee7b5e
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-06-19 23:36:49 +0200

    Added a test project

---
+++ Docxes Testing/src/BusinessLogic/SchoolProcessorTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VrankenBischof.Docxes;
+using VrankenBischof.Docxes.BusinessLogic;
+
+namespace DocxesTesting {
+
+    [TestClass]
+    public class SchoolProcessorTest {
+
+        private static string GetRandomEntityName() {
+            return "[AUTOMATED TESTING] " + new Guid().GetHashCode();
+        }
+
+        [TestMethod]
+        public void TestMethod1() {
+            // TODO: __DV
+
+            // Arrange
+            var SchoolProcessor = new SchoolProcessor();
+
+            // Act & Assert
+            var schoolToSave = new School(GetRandomEntityName(), "Test comment");
+            SchoolProcessor.Create(schoolToSave);
+
+            //Update
+
+            //Delete
+        }
+
+    }
+
+}
